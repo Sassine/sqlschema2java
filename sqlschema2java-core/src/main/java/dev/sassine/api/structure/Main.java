aeb8ecc32645c989441c0a62daa208435ce7fa50
Sassine El-Asmar <sassineasmar@gmail.com>
2022-04-18 17:50:29 -0300

    fix: error build static method lombok builder

--- sqlschema2java-core/src/main/java/dev/sassine/api/structure/Main.java
+++ sqlschema2java-core/src/main/java/dev/sassine/api/structure/Main.java
@@ -1,15 +1,16 @@
 package dev.sassine.api.structure;
 
 import static dev.sassine.api.structure.Sqlschema2Java.generate;
-import static dev.sassine.api.structure.model.java.EnvironmentModel.builder;
 import static java.nio.file.Paths.get;
 
 import java.io.FileNotFoundException;
 
+import dev.sassine.api.structure.model.java.EnvironmentModel;
+
 public class Main {
 
 	public static void main(final String[] args) throws FileNotFoundException {
-		generate(builder()
+		generate(EnvironmentModel.builder()
 				.packageName("dev.sassine.api.structure.generated")
 				.sourceDirectory(get("src","test","resources","default.sql").toFile().getAbsolutePath())
 				.build());
